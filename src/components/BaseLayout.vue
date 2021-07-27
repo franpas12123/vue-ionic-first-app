f1837d02e0868f52551a662150dd97e1f68daee7
Frances Pasulohan <franpas12123@gmail.com>
2021-07-27 20:37:24 +0800

    created base layout for ionic pages

---
+++ src/components/BaseLayout.vue
@@ -0,0 +1,51 @@
+<template>
+    <ion-page>
+        <ion-header :translucent="true">
+            <ion-toolbar>
+                <ion-title>{{ pageTitle }}</ion-title>
+            </ion-toolbar>
+        </ion-header>
+        <ion-content :fullscreen="true">
+            <slot />
+        </ion-content>
+    </ion-page>
+</template>
+
+<script>
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from "@ionic/vue";
+export default {
+    props: ['pageTitle'],
+    components:{
+        IonPage, IonHeader, IonToolbar, IonTitle, IonContent
+    }
+}
+</script>
+<style scoped>
+#container {
+  text-align: center;
+  
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 50%;
+  transform: translateY(-50%);
+}
+
+#container strong {
+  font-size: 20px;
+  line-height: 26px;
+}
+
+#container p {
+  font-size: 16px;
+  line-height: 22px;
+  
+  color: #8c8c8c;
+  
+  margin: 0;
+}
+
+#container a {
+  text-decoration: none;
+}
+</style>
