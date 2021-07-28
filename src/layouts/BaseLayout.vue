7a6255e80937d5b53c9fd10b8650cacd5c23745d
Frances Pasulohan <franpas12123@gmail.com>
2021-07-28 23:11:14 +0800

    moved to /layouts

---
+++ src/layouts/BaseLayout.vue
@@ -0,0 +1,63 @@
+<template>
+  <ion-page>
+    <!-- <Sidebar></Sidebar> -->
+    <ion-header :translucent="true" v-if="pageTitle != 'Login'">
+      <ion-toolbar>
+        <ion-title
+          ><strong>{{ pageTitle }}</strong></ion-title
+        >
+        <ion-buttons slot="start">
+          <ion-menu-button autoHide="false"></ion-menu-button>
+        </ion-buttons>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content :fullscreen="true">
+      <slot />
+    </ion-content>
+  </ion-page>
+</template>
+
+<script>
+import {
+  IonPage,
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonMenuButton,
+  IonButtons,
+  // menuController,
+} from '@ionic/vue';
+import { mapActions } from 'vuex';
+// import Sidebar from './SidebarLayout';
+
+export default {
+  props: ['pageTitle'],
+  components: {
+    IonPage,
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonMenuButton,
+    IonButtons,
+    // Sidebar,
+  },
+  data() {
+    return {};
+  },
+  methods: {
+    ...mapActions(['getStudent']),
+    // openSidebar() {
+    //   menuController.enable(true, 'first');
+    //   menuController.open('first');
+    //   console.log('opensidebar');
+    // },
+  },
+  created() {
+    console.log('student');
+    console.log(this.getStudent);
+    console.log(this.$store.state);
+  },
+};
+</script>
