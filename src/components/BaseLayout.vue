61faf0cc7ccc319a1aa64a6a6969c8b70068e4c1
Frances Pasulohan <franpas12123@gmail.com>
2021-07-28 17:44:11 +0800

    removed unused styling

--- src/components/BaseLayout.vue
+++ src/components/BaseLayout.vue
@@ -1,54 +1,42 @@
 <template>
-    <ion-page>
-        <ion-header :translucent="true">
-            <ion-toolbar>
-                <ion-buttons slot="start">
-                    <ion-menu-button></ion-menu-button>
-                </ion-buttons>
-                <ion-title>{{ pageTitle }}</ion-title>
-            </ion-toolbar>
-        </ion-header>
-        <ion-content :fullscreen="true">
-            <slot />
-        </ion-content>
-    </ion-page>
+  <ion-page>
+    <ion-header :translucent="true">
+      <ion-toolbar>
+        <ion-buttons slot="start">
+          <ion-menu-button></ion-menu-button>
+        </ion-buttons>
+        <ion-title>{{ pageTitle }}</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content :fullscreen="true">
+      <slot />
+    </ion-content>
+  </ion-page>
 </template>
 
 <script>
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonMenuButton, IonButtons } from "@ionic/vue";
+import {
+  IonPage,
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonMenuButton,
+  IonButtons,
+} from '@ionic/vue';
 export default {
-    props: ['pageTitle'],
-    components:{
-        IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonMenuButton, IonButtons
-    }
-}
+  props: ['pageTitle'],
+  components: {
+    IonPage,
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonMenuButton,
+    IonButtons,
+  },
+  data() {
+    return {};
+  },
+};
 </script>
-<style scoped>
-#container {
-  text-align: center;
-  
-  position: absolute;
-  left: 0;
-  right: 0;
-  top: 50%;
-  transform: translateY(-50%);
-}
-
-#container strong {
-  font-size: 20px;
-  line-height: 26px;
-}
-
-#container p {
-  font-size: 16px;
-  line-height: 22px;
-  
-  color: #8c8c8c;
-  
-  margin: 0;
-}
-
-#container a {
-  text-decoration: none;
-}
-</style>
